35b51aa0c0348e22164bbef8b193532061d1eadb
Philippe Le Van (@plv) <philippe.levan@kitpages.fr>
2012-05-31 12:01:30 +0200

    beginning travis-ci

---
+++ Tests/PhpunitTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Kitpages\DataGridBundle\Tests;
+
+class PhpunitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPhpunit()
+    {
+        $this->assertTrue(true);
+    }
+}

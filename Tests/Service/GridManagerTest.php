b7e0d3b683e63ba8321848c8f53676096815be90
Philippe Le Van (@plv) <philippe.levan@kitpages.fr>
2012-08-28 14:53:46 +0200

    added unit tests for groupby requests

--- Tests/Service/GridManagerTest.php
+++ Tests/Service/GridManagerTest.php
@@ -56,6 +56,43 @@ class GridManagerTest extends BundleOrmTestCase
         $this->assertEquals(2, $paginator->getNextButtonPage());
     }
 
+    public function testPaginatorGroupBy()
+    {
+        // create queryBuilder
+        $em = $this->getEntityManager();
+        $repository = $em->getRepository('Kitpages\DataGridBundle\Tests\TestEntities\Node');
+        $queryBuilder = $repository->createQueryBuilder("node");
+        $queryBuilder->select("node.user, count(node.id) as cnt");
+        $queryBuilder->groupBy("node.user");
+
+        // create EventDispatcher mock
+        $service = $this->getMock('Symfony\Component\EventDispatcher\EventDispatcher');
+        // create Request mock (ok this is not a mock....)
+        $request = new \Symfony\Component\HttpFoundation\Request();
+        $_SERVER["REQUEST_URI"] = "/foo";
+
+        // create gridManager instance
+        $gridManager = new GridManager($service);
+
+        // configure paginator
+        $paginatorConfig = new PaginatorConfig();
+        $paginatorConfig->setCountFieldName("node.user");
+        $paginatorConfig->setItemCountInPage(3);
+
+        // get paginator
+        $paginator = $gridManager->getPaginator($queryBuilder, $paginatorConfig, $request);
+
+        // tests
+        $this->assertEquals(5, $paginator->getTotalItemCount());
+
+        $this->assertEquals(2, $paginator->getTotalPageCount());
+
+        $this->assertEquals(array(1,2), $paginator->getPageRange());
+
+        $this->assertEquals(1 , $paginator->getCurrentPage());
+        $this->assertEquals(2, $paginator->getNextButtonPage());
+    }
+
     public function initGridConfig()
     {
         // configure paginator

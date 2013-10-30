60969cfb245174965b447ddfee051f11d1b79fa3
Philippe Le Van (@plv) <philippe.levan@kitpages.fr>
2013-10-30 13:33:49 +0100

    fix after init pb with datagrid

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('kitpages_data_grid');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
